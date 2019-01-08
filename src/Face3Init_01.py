92a33e07a67a683641881bd6f245b4b435139b31
Thanh Tran <thanhtc@nextsmarty.com>
2019-01-08 11:32:39 +0700

    Fix bug NS-148

--- src/Face3Init_01.py
+++ src/Face3Init_01.py
@@ -13,6 +13,13 @@ def Face3Init_01(model_def_file, resume_file):
         print('Need a resume file')
         exit()
     solver = T1_SolverParser(model_def_file, resume_file)
+    if solver['solver_mode'] == 'GPU':
+        caffe.set_mode_gpu()
+        caffe.set_device(solver['device_id'])
+    else:
+        caffe.set_mode_cpu()
+    net = caffe.Net(solver['net'], caffe.TEST)
+    layers = solver['model']
 
     for i in range(len(layers)):
         layer_name = layers[i]['layer_names'][0][0]
@@ -23,13 +30,5 @@ def Face3Init_01(model_def_file, resume_file):
         # print(net.params[layer_name][0].data.shape)
         net.params[layer_name][0].data[:] = np.transpose(layer_weight)
         net.params[layer_name][1].data[:] = np.squeeze()
-
-    if solver['solver_mode'] == 'GPU':
-        caffe.set_mode_gpu()
-        caffe.set_device(solver['device_id'])
-    else:
-        caffe.set_mode_cpu()
-    net = caffe.Net(solver['net'], caffe.TEST)
-    layers = solver['model']
 
     return solver, net
